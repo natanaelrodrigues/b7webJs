10b180bb670cd6606ee1f96fbbdba4624591eda6
Natanael  R. Rodigues <natanaelrodrigues@gmail.com>
2020-12-09 11:40:26 -0300

    Upload fotos

---
+++ node/p1/middlewares/imageMiddeware.js
@@ -0,0 +1,38 @@
+const multer = require('multer');
+const jimp = require('jimp');
+const { v4: uuidv4 } = require('uuid');
+
+
+const multerOptions = {
+    storage:multer.memoryStorage(),
+    fileFilter:(req, file, next)=>{
+        const allowed = ['image/jpeg','image/jpg','impage/png'];
+        if(allowed.includes(file.mimetype)){
+            next(null, true);
+        } else{
+            next({message: 'Arquivo não suportado'}, false);
+        }
+    }
+}
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+    if(!req.file){
+        next();
+        return
+    };
+
+    const ext = req.file.mimetype.split('/')[1];
+
+    let filename = `${uuidv4()}.${ext}`;
+
+    req.body.photo = filename;
+
+    const photo = await jimp.read(req.file.buffer);
+    await photo.resize(800, jimp.AUTO);
+
+    await photo.write(`./public/media/${filename}`);
+
+    next();
+};
--- node/p1/routes/index.js
+++ node/p1/routes/index.js
@@ -3,12 +3,17 @@ const homeController = require('../controllers/homeController')
 const usersController = require('../controllers/userController')
 const postController = require('../controllers/postController.js')
 
+const imageMiddleware = require('../middlewares/imageMiddeware');
+
 const router = express.Router();
 router.get('/', homeController.index);
 router.get('/users/login',usersController.login);
 
 router.get('/post/add',postController.add);
-router.post('/post/add',postController.addAction);
+router.post('/post/add',
+            imageMiddleware.upload, 
+            imageMiddleware.resize,
+            postController.addAction);
 
 router.get('/post/:slug/edit',postController.edit)
 router.post('/post/:slug/edit',postController.editAction)
